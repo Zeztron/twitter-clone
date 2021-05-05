b22c61ebe1ff71ede11a756dafb4fd3226b704d4
Zeztron <pateltharsh@gmail.com>
2021-05-05 14:42:03 -0400

    added setContext for auth

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,11 +1,29 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
+import {
+  ApolloProvider,
+  ApolloClient,
+  InMemoryCache,
+  HttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 import Users from './Users';
 import Landing from './Landing';
 
+const httpLink = new HttpLink({ uri: 'http://localhost:4000' });
+const authLink = setContext(async (req, { headers }) => {
+  const token = localStorage.getItem('token');
+
+  return {
+    headers: {
+      ...headers,
+      Authorization: token ? `Bearer ${token}` : null,
+    },
+  };
+});
+
 const client = new ApolloClient({
-  uri: 'http://localhost:4000',
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
